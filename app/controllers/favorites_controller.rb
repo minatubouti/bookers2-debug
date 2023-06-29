4d98eb8fdcbf4be5bbbd5fe4210f9db7eb38d412
minatubouti <pingneimeinai@gmail.com>
2023-06-29 05:35:35 +0000

    third commit

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,13 +3,13 @@ class FavoritesController < ApplicationController
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.new(book_id: book.id)
     favorite.save
-    redirect_to book_path(book)
+    redirect_to request.referer
   end
   
   def destroy
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.find_by(book_id: book.id)
     favorite.destroy
-    redirect_to book_path(book)
+    redirect_to request.referer
   end
 end
