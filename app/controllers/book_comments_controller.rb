21ed284cdb0a666479118da5454c53085eaeed19
minatubouti <pingneimeinai@gmail.com>
2023-07-03 14:21:39 +0000

    eighth commit

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -3,13 +3,14 @@ class BookCommentsController < ApplicationController
     book = Book.find(params[:book_id])
     comment = current_user.book_comments.new(book_comment_params)
     comment.book_id = book.id
-    comment.save
-    redirect_to book_path(book)
+    comment.save 
+  #  redirect_to book_path(book)
   end
   
   def destroy
     BookComment.find(params[:id]).destroy
-    redirect_to book_path(params[:book_id])
+    @book = Book.find(params[:book_id])
+   # redirect_to book_path(params[:book_id])
   end
   
   private
--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,7 +4,7 @@ class FavoritesController < ApplicationController
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.new(book_id: book.id)
     favorite.save
-    render json: { book_id: @book.id, partial: render_to_string(partial: 'favorites/favorite-btn', formats: [:html], locals: { book: @book }) }
+   
   end
   
   def destroy
@@ -12,6 +12,6 @@ class FavoritesController < ApplicationController
     book = Book.find(params[:book_id])
     favorite = current_user.favorites.find_by(book_id: book.id)
     favorite.destroy
-    render json: { book_id: @book.id, partial: render_to_string(partial: 'favorites/favorite-btn', formats: [:html], locals: { book: @book }) }
+    
   end
 end
