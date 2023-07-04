1ad88e283027eb39509fec3d3a5458ada466b17f
minatubouti <pingneimeinai@gmail.com>
2023-07-04 14:14:11 +0000

    ninth commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          
+  has_many :books, dependent: :destroy
   has_many :book_images, dependent: :destroy
   has_many :book_comments, dependent: :destroy
   has_one_attached :profile_image
   has_many :favorites, dependent: :destroy
+# has_many :favorited_books, through: :favorites, source: :book
+   
   
   #フォローしている
   has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
